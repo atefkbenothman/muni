9e51205a309ea9e2f0e99d6ea32c71eac89d28bd
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-02-21 22:29:30 -0800

    styles(general): add geist mono font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter, Geist_Mono } from "next/font/google"
+import "./globals.css"
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-});
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "SF Muni Map",
   description: "interactive san francisco muni app",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${geistMono.variable} dark antialiased`}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
