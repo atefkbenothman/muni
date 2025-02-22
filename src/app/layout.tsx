89798e2667ab6242686da8911984d2e1214a1684
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-02-21 23:08:31 -0800

    styles(favicon): add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next"
-import { Inter, Geist_Mono } from "next/font/google"
+import { Geist_Mono } from "next/font/google"
 import "./globals.css"
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -24,6 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📍</text></svg>"
+        ></link>
+      </head>
       <body className={`${geistMono.variable} dark antialiased`}>
         {children}
       </body>
