dc67e0fdafe050cbe650f8324c249721c8abf14e
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-02-22 23:08:54 -0800

    ui(general): fix rounded corners

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,8 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground shadow-xs",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
@@ -22,8 +21,8 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-7 rounded-sm px-0 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-sm px-6 has-[>svg]:px-4",
+        sm: "h-7 rounded-xs px-0 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-xs px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
